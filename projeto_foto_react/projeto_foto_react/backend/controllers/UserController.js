ba1475529d8e97bc761d9f5d7ecf64765ad295d0
Gabiru1089 <gabriel1m52020@gmail.com>
2025-05-29 14:59:33 -0300

    User Controller

--- projeto_foto_react/projeto_foto_react/backend/controllers/UserController.js
+++ projeto_foto_react/projeto_foto_react/backend/controllers/UserController.js
@@ -1,14 +1,33 @@
 import db from "../db.js";
 
 export const addUser = (req, res) => {
-  const nome = req.body.nome;
+  const {email, telefone, senha_hash, interesses} = req.body
   const imagem = req.file ? `/uploads/${req.file.filename}` : null;
-
-  const query = "INSERT INTO users (nome, imagem) VALUES (?, ?)";
-  db.query(query, [nome, imagem], (err, result) => {
+  const query = "INSERT INTO usuarios (imagem, email, telefone, senha_hash, interesses) VALUES (?, ?, ?, ?, ?)";
+  db.query(query, [imagem, email, telefone, senha_hash, interesses], (err, result) => {
     if (err) return res.status(500).json(err);
     return res.status(200).json({ message: "Usuário adicionado com sucesso" });
   });
+};
+
+export const addCPF = (req, res) => {
+  const {email, telefone, senha_hash, interesses, cpf} = req.body;
+  addUser({email , telefone, senha_hash, interesses}, res);
+  const query = `INSERT INTO pessoa_fisica (id, cpf) VALUES ((SELECT id FROM usuarios WHERE email = ?), ?)`;
+  db.query(query, [email, cpf], (err, results) => {
+    if (err) return res.status(500).json(err);
+    return res.status(200).json({ message : `CPF adicionado com sucesso:`, affectedRows: results.affectedRows});
+  });
+};
+
+export const addCNPJ = (req, res) =>{
+  const {email, telefone, senha_hash, interesses, cnpj} = req.body;
+  addUser({email , telefone, senha_hash, interesses}, res);
+  const query = `INSERT INTO pessoa_juridica (id, cnpj) VALUES ((SELECT id FROM usuarios WHERE email = ?), ?)`;
+  db.query(query, [email, cnpj], (err, results) =>{
+    if (err) return res.status(500).json(err);
+    return res.status(200).json({message: `CNPJ adicionado com sucesso:`, affectedRows: results.affectedRows});
+  }); 
 };
 
 export const getUsers = (req, res) => {
